4335e802c86c574262d8729a0d70121e7f2c6754
saberdance <603502393@qq.com>
2020-07-18 17:46:55 +0800

    Fix Mac Syscall Bug

--- src/UsbMonitor/usbMonitor.go
+++ src/UsbMonitor/usbMonitor.go
@@ -1,45 +1,9 @@
 package usbmonitor
 
 import (
-	"fmt"
-	"os"
 	"sync"
-	"syscall"
 	"time"
-	"unsafe"
 )
-
-func IntPtr(n int) uintptr {
-	return uintptr(n)
-}
-
-func StrPtr(s string) uintptr {
-	return uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(s)))
-}
-
-func CheckUsbCert() (int, error) {
-	dir, _ := os.Getwd()
-	dllPath := dir + "\\uc\\uc.dll"
-	fmt.Printf("dllPath: %s\n", dllPath)
-	handle, err := syscall.LoadLibrary(dllPath)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return -1, err
-	}
-	defer syscall.FreeLibrary(handle)
-	checker, err := syscall.GetProcAddress(handle, "CheckUSBReady")
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return -2, err
-	}
-	ret, _, _ := syscall.Syscall(checker, 0, 0, 0, 0)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return -3, err
-	}
-	fmt.Println("检查USB结果:", ret)
-	return int(ret), err
-}
 
 //证书监控模块
 type Monitor struct {
@@ -59,26 +23,11 @@ func Instance() *Monitor {
 	return instance
 }
 
-func ListenFunc() {
-	for {
-		ret, _ := CheckUsbCert()
-		Channel <- ret
-		time.Sleep(time.Second * 2)
-	}
-}
-
 func Fake_ListenFunc(needret int) {
 	for {
 		Channel <- needret
 		time.Sleep(time.Second * 2)
 	}
-}
-
-func StartListen() {
-	once.Do(func() {
-		Channel = make(chan int)
-		go ListenFunc()
-	})
 }
 
 func Fake_StartListen(needret int) {
---
+++ src/UsbMonitor/usbhelper.go
@@ -0,0 +1,58 @@
+//+build windows
+
+package usbmonitor
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"time"
+	"unsafe"
+)
+
+func IntPtr(n int) uintptr {
+	return uintptr(n)
+}
+
+func StrPtr(s string) uintptr {
+	return uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(s)))
+}
+
+func CheckUsbCert() (int, error) {
+	dir, _ := os.Getwd()
+	dllPath := dir + "\\uc\\uc.dll"
+	fmt.Printf("dllPath: %s\n", dllPath)
+	handle, err := syscall.LoadLibrary(dllPath)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return -1, err
+	}
+	defer syscall.FreeLibrary(handle)
+	checker, err := syscall.GetProcAddress(handle, "CheckUSBReady")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return -2, err
+	}
+	ret, _, _ := syscall.Syscall(checker, 0, 0, 0, 0)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return -3, err
+	}
+	fmt.Println("检查USB结果:", ret)
+	return int(ret), err
+}
+
+func ListenFunc() {
+	for {
+		ret, _ := CheckUsbCert()
+		Channel <- ret
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func StartListen() {
+	once.Do(func() {
+		Channel = make(chan int)
+		go ListenFunc()
+	})
+}
