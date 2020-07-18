b33a06ffa79b4484fc80a79212ab7f5c01b8a1a2
saberdance <603502393@qq.com>
2020-07-18 18:41:47 +0800

    Add usbmonitor.Fake_GetPrivateKey(needret string)

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	// 	"time"
 	"fmt"
-	"time"
 
 	usbmonitor "github.com/saberdance/bc/src/UsbMonitor"
 )
 
 func main() {
 	//Fake_StartListen 的参数为需要Channel返回的值
-	usbmonitor.Fake_StartListen(0)
-	for {
-		x := <-usbmonitor.Channel
-		fmt.Printf("Channel Ret: %d\n", x)
-		time.Sleep(time.Second * 2)
-	}
+	// usbmonitor.Fake_StartListen(0)
+	// for {
+	// 	x := <-usbmonitor.Channel
+	// 	fmt.Printf("Channel Ret: %d\n", x)
+	// 	time.Sleep(time.Second * 2)
+	// }
+	x := usbmonitor.Fake_GetPrivateKey("")
+	y := usbmonitor.Fake_GetPrivateKey("1122334455")
+	fmt.Printf("fake: %s\n", x)
+	fmt.Printf("fake: %s\n", y)
 }
--- src/UsbMonitor/usbMonitor.go
+++ src/UsbMonitor/usbMonitor.go
@@ -1,6 +1,9 @@
 package usbmonitor
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -35,4 +38,30 @@ func Fake_StartListen(needret int) {
 		Channel = make(chan int)
 		go Fake_ListenFunc(needret)
 	})
+}
+
+//生成随机字符串
+func GetRandomString(length int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+// 生成32位MD5
+func MD5(text string) string {
+	ctx := md5.New()
+	ctx.Write([]byte(text))
+	return hex.EncodeToString(ctx.Sum(nil))
+}
+
+func Fake_GetPrivateKey(needret string) string {
+	if needret == "" {
+		return GetRandomString(32)
+	}
+	return needret
 }
